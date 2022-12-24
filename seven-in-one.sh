43a07b51289d8f3df4b85297d8c990280dc4688b
Mustafa <travellingtypist1@gmail.com>
2022-12-24 14:05:32 +0000

    add _helpers

--- seven-in-one.sh
+++ seven-in-one.sh
@@ -1,4 +1,9 @@
-mkdir vendors base layout components themes abstracts
+# exit when any command fails
+set -e
+
+cd styles
+
+mkdir vendors base layout components themes abstracts pages 
 
 cd abstracts
 touch _functions.scss _mixins.scss _variables.scss
@@ -6,5 +11,7 @@ touch _functions.scss _mixins.scss _variables.scss
 cd ..
 cd base
 touch _animations.scss _base.scss _typography.scss _utilities.scss
-cp $HOME/bin/resources/base.scss ./_base.scss
-
+cp $HOME/bin/seven-in-one/resources/_base.scss ./_base.scss
+cp $HOME/bin/seven-in-one/resources/_helpers.scss ./_helpers.scss
+cd ..
+cd ..
